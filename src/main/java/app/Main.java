8e340d07b9da45448e2976065c0093e3f2b5367e
storebe <storebe@icloud.com>
2025-11-25 12:34:08 -0500

    new code structure with UI, following todolist format

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,22 @@
+package app;
+
+import javax.swing.*;
+
+public class Main {
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("UofT Course Explorer & Planner");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Our UI is now a panel:
+        frame.add(new CourseExplorerPanel());
+
+        frame.pack();
+        frame.setLocationRelativeTo(null); // center
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::createAndShowGUI);
+    }
+}
