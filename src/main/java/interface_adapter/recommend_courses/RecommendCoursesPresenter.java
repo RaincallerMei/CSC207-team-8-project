f5385f3e0e9980382a7df46d92d415687860dcb6
harrymei <harrymei2006@gmail.com>
2025-11-28 13:33:24 -0500

    Controller

--- src/main/java/interface_adapter/recommend_courses/RecommendCoursesPresenter.java
+++ src/main/java/interface_adapter/recommend_courses/RecommendCoursesPresenter.java
@@ -1,4 +1,22 @@
 package interface_adapter.recommend_courses;
 
-public class RecommendCoursesPresenter {
+import use_case.recommend_courses.RecommendCoursesOutputBoundary;
+import use_case.recommend_courses.RecommendCoursesOutputData;
+
+public class RecommendCoursesPresenter implements RecommendCoursesOutputBoundary {
+    private final RecommendCoursesViewModel viewModel;
+
+    public RecommendCoursesPresenter(RecommendCoursesViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(RecommendCoursesOutputData outputData) {
+        viewModel.setRecommendedCourses(outputData.getRecommendedCourses());
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage) {
+        viewModel.fireError(errorMessage);
+    }
 }
--- src/main/java/interface_adapter/recommend_courses/RecommendCoursesViewModel.java
+++ src/main/java/interface_adapter/recommend_courses/RecommendCoursesViewModel.java
@@ -1,4 +1,31 @@
 package interface_adapter.recommend_courses;
 
+import entity.Course;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecommendCoursesViewModel {
+    public static final String PROPERTY_RECOMMENDATIONS = "recommendations";
+    public static final String PROPERTY_ERROR = "error";
+
+    private List<Course> recommendedCourses = new ArrayList<>();
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public List<Course> getRecommendedCourses() { return recommendedCourses; }
+
+    public void setRecommendedCourses(List<Course> recommendedCourses) {
+        List<Course> old = this.recommendedCourses;
+        this.recommendedCourses = recommendedCourses;
+        support.firePropertyChange(PROPERTY_RECOMMENDATIONS, old, recommendedCourses);
+    }
+
+    public void fireError(String error) {
+        support.firePropertyChange(PROPERTY_ERROR, null, error);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 }
