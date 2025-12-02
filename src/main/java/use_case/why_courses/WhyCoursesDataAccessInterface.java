c75b058ae3f3b9942a71a0263f6f7de0db623aa0
storeBE <111542916+storeBE@users.noreply.github.com>
2025-12-01 23:48:39 -0500

    Revert "Arthur2"

--- src/main/java/use_case/why_courses/WhyCoursesDataAccessInterface.java
+++
@@ -1,5 +0,0 @@
-package use_case.why_courses;
-
-public interface WhyCoursesDataAccessInterface {
-    String getReasonForCourse(String courseCode);
-}
--- src/main/java/use_case/why_courses/WhyCoursesInputBoundary.java
+++
@@ -1,5 +0,0 @@
-package use_case.why_courses;
-
-public interface WhyCoursesInputBoundary {
-    void execute(WhyCoursesInputData inputData);
-}
